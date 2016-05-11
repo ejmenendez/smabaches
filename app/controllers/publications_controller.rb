6c79d18d22d301f6731ca292c94e259da92d9ca1
Esteban Menéndez <ejmenendez@gmail.com>
2016-05-11 16:49:34 -0300

    ABM de publicaciones

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -4,8 +4,50 @@ class PublicationsController < ApplicationController
     @publication = Publication.all
   end
 
+    def show
+        @publication = Publication.find(params[:id]) 
+    end
+    
+    def new
+        @publication = Publication.new    
+    end
+    
   def create
-
+      @publication = Publication.new(publication_params)
+      
+      begin
+          @publication.save!
+          redirect_to @publication
+      rescue ActiveRecord::RecordInvalid
+          render 'new'
+      end
   end
+    
+    def destroy
+        publication = Publication.find(params[:id])
+        publication.destroy
+        redirect_to publications_path
+    end
+    
+    def edit
+        @publication = Publication.find(params[:id])
+    end
+    
+    def update
+        @publication = Publication.new(publication_params)
+      
+      begin
+          @publication.update!(publication_params)
+          redirect_to @publication
+      rescue ActiveRecord::RecordInvalid
+          render 'edit'
+      end
+    end
 
+    private
+    
+    def publication_params
+        params.require(:publication).permit(:description, :latitude, :longitude)    
+    end
+    
 end
