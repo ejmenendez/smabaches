64de97c2582f0945e26f30c683dda5b9146ff043
pochi94 <rodrigo.94.chavez@gmail.com>
2016-05-18 01:08:17 -0300

    Implementacion bootstrap y layouts

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,5 +1,5 @@
 class PublicationsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @publication = Publication.all
   end
