b159e7471f81c1aedd2306aa199ce541cdb4610c
Esteban Menéndez <ejmenendez@gmail.com>
2016-05-24 18:29:23 -0300

    agregado autorización

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
+  include Pundit
+  
+  # en caso que haya excepciones con las autorizaciones de las distintas acciones
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  private
+  
+  def user_not_authorized
+		flash[:alert] = "You are not authorized to perform this action."
+		redirect_to(request.referrer || root_path)
+  end
+  
 end
--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,61 +1,74 @@
 class PublicationsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  
-  def index
-    @publications = Publication.all  
-    @hash = create_markers(@publications)
-  end
+	skip_before_action :authenticate_user!, only: [:index, :show]
 
-    def show
-        @publication = Publication.find(params[:id])
-        @hash = create_markers(@publication)
-    end
+	def index
+		authorize Publication
+		@publications = Publication.all
+		# crear los marcadores para el google maps  
+		@hash = create_markers(@publications)
+	end
 
-    def new
-        @publication = Publication.new
-    end
-
-  def create
-      @publication = Publication.new(publication_params)
-      @publication.author = current_user
-
-      begin
-          @publication.save!
-          redirect_to @publication
-      rescue ActiveRecord::RecordInvalid
-          render 'new'
-      end
-  end
-
-    def destroy
-        publication = Publication.find(params[:id])
-        publication.destroy
-        redirect_to publications_path
-    end
-
-    def edit
-        @publication = Publication.find(params[:id])
-        @hash = create_markers(@publication)
-    end
-
-    def update
-        @publication = Publication.find(params[:id])
+	def show
+		@publication = Publication.find(params[:id])
+		authorize @publication
+		# crea el marcador google maps de la publicación actual
 		@hash = create_markers(@publication)
-      begin
-          @publication.update!(publication_params)
-          redirect_to @publication
-      rescue ActiveRecord::RecordInvalid
-          render 'edit'
-      end
-    end
+	end
 
-    private
+	def new
+		authorize Publication
+		@publication = Publication.new
+	end
 
-    def publication_params
-        params.require(:publication).permit(:description, :latitude, :longitude, :title, :published)
-    end
-    
-    def create_markers(publications)
+	def create
+	  @publication = Publication.new(publication_params)
+	  @publication.author = current_user
+	  authorize @publication
+	  
+	  begin
+		  @publication.save!
+		  redirect_to @publication
+	  rescue ActiveRecord::RecordInvalid
+		  render 'new'
+	  end
+	end
+
+	def destroy
+		@publication = Publication.find(params[:id])
+		authorize @publication
+		
+		@publication.destroy
+		redirect_to publications_path
+	end
+
+	def edit
+		@publication = Publication.find(params[:id])
+		authorize @publication
+		# crea el marcador google maps de la publicación actual
+		@hash = create_markers(@publication)
+		
+	end
+
+	def update
+		@publication = Publication.find(params[:id])
+		authorize @publication
+		# crea el marcador google maps de la publicación actual
+		@hash = create_markers(@publication)
+	  begin
+		  @publication.update!(publication_params)
+		  redirect_to @publication
+	  rescue ActiveRecord::RecordInvalid
+		  render 'edit'
+	  end
+	end
+
+	private
+
+	def publication_params
+		params.require(:publication).permit(:description, :latitude, :longitude, :title, :published)
+	end
+
+	def create_markers(publications)
 		 Gmaps4rails.build_markers(publications) do |publication, marker|
 			marker.lat publication.latitude
 			marker.lng publication.longitude
