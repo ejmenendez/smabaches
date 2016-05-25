0e5c59fb09329c654dd6cf03064307560b71f8cb
Esteban Menéndez <ejmenendez@gmail.com>
2016-05-25 18:11:22 -0300

    migration latitud longitud

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524200450) do
+ActiveRecord::Schema.define(version: 20160525210631) do
 
   create_table "photos", force: true do |t|
     t.string   "file"
