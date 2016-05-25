b1fc1ffc0c2b69a4f2295680f4646e22eff74c57
Esteban Menéndez <ejmenendez@gmail.com>
2016-05-25 17:03:28 -0300

    envío de mail funcionando, cambio de url a la aplicación

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # emails
-  config.action_mailer.default_url_options = {:host => 'smtp.mailgun.org' }
+  config.action_mailer.default_url_options = {:host => 'https://smabaches-menendez.rhcloud.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   :port           => 587,
