6db715313187160b3205695373f2b787574c6947
pochi94 <rodrigo.94.chavez@gmail.com>
2016-05-28 18:43:42 -0300

    Visualizacion mapa y foto publicacion

--- app/models/photo_uploader.rb
+++ app/models/photo_uploader.rb
@@ -10,7 +10,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   #storage :fog
-
+  #process :resize_to_fill => [400, 600]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   #def store_dir
@@ -26,16 +26,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
+  #process :scale => [200, 300]
+
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [50, 50]
-  end
+  #version :thumb do
+  #  process :resize_to_fill => [200, 200]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -56,6 +56,5 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def cache_dir
 		'tmp/smabaches-cache'
   end
-
 
 end
