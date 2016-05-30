7535c211d535b5a660849f18ff4f128cb58b0707
Esteban Menéndez <ejmenendez@gmail.com>
2016-05-30 19:07:21 -0300

    20160530 funcionando

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,6 +1,6 @@
 class Publication < ActiveRecord::Base
   
-  validates :author, presence:true
+  validates :author, :title, :description, :photo, :latitude, :longitude, presence: true
   belongs_to :author, class_name: 'User'
   
   mount_uploader :photo, PhotoUploader, ignore_integrity_errors: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,13 @@ estebanUser = User.new(
 estebanUser.save!
 estebanUser.add_role :admin
 
+user = User.new(
+  :email                 => "esteban@example.com",
+  :password              => "12345678",
+  :password_confirmation => "12345678"
+)
+estebanUser.save!
+
 Publication.create!(title: 'Primera Publicacion',description: 'Bache molesto', author: rodriUser, latitude: -40.1362682 , longitude: -71.29446349999999);
 Publication.create!(title: 'Segunda Publiacion',description: 'Bache molesto', author: estebanUser, latitude: -40.14 , longitude: -71.295);
 Publication.create!(title: 'Tercera Publicacion',description: 'Bache molesto',author: estebanUser, latitude: -40.15 , longitude: -71.296);
