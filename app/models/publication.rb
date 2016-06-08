5b88d1d19a59239d7d40344f4542800393225707
pochi94 <rodrigo.94.chavez@gmail.com>
2016-06-08 11:31:41 -0300

    mejoras en vista index y show publicaciones

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,10 +1,10 @@
 class Publication < ActiveRecord::Base
   #para la gema commontator
   acts_as_commontable
-  
-  validates :author, :title, :description, :latitude, :longitude, presence: true #:photo, 
+
+  validates :author, :title, :description, :latitude, :longitude, presence: true #:photo,
   belongs_to :author, class_name: 'User'
-    
-    has_attached_file :photo, styles: {medium: "300x300>", thumb: "100x100>"}, default_url: "/images/:style/missing.png"
+
+    has_attached_file :photo, styles: {medium: "400x400#", thumb: "200x200#"}, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
