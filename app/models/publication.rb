9db367187f9b7ac0e21ab9be5f54a3b08e567ed1
pochi94 <rodrigo.94.chavez@gmail.com>
2016-06-09 12:12:37 -0300

    modificacion maps windows resize

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -7,4 +7,5 @@ class Publication < ActiveRecord::Base
 
     has_attached_file :photo, styles: {medium: "400x400#", thumb: "200x200#"}, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+     
 end
