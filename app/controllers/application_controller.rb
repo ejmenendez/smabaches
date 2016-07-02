1cb2155a5b85e9b0d337e9e0a4d931cfa033a9ad
pochi94 <rodrigo.94.chavez@gmail.com>
2016-07-01 22:33:28 -0300

    cambio en los flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   include Pundit
-  
+
   # en caso que haya excepciones con las autorizaciones de las distintas acciones
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
+
   private
-  
+
   def user_not_authorized
-		flash[:alert] = "You are not authorized to perform this action."
+		flash.new[:alert] = "No esta autorizado para realizar esta accion"
 		redirect_to(request.referrer || root_path)
   end
-  
+
 end
